04abe8683f10e2eea4dc396acf48a355027db21d
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 22:29:16 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
     arr.sort_by do |word|
       word.each do |letter|
-        puts letter 
+        puts letter
       end
     end
 
