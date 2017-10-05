596ed6d6c30ec729dc46bdd31a7556a4e4b391c7
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 22:19:21 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,11 @@
 def alphabetize(arr)
-  esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
-  arr.sort_by{|string| string.tr(esp_alph, ascii)}
+  newAlphabetArr = "a b c ĉ d e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z"
+    arr.sort_by do
+      |word|
+      word.split('').collect do
+        |letter|
+        newAlphabetArr.index(letter)
+      end
+    end
+  end
 end
