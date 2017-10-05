ed3f2212f0bc62fb075f8c1fe934ca1bc75a35d2
Brendan McIlhenny <bmcilhen@gmail.com>
2017-10-05 22:20:33 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,9 @@
 def alphabetize(arr)
-  newAlphabetArr = "a b c ĉ d e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z"
-    arr.sort_by do
-      |word|
-      word.split('').collect do
-        |letter|
+  esperanto_alphabet = "a b c ĉ d e f g ĝ h ĥ i j ĵ k l m n o p r s ŝ t u ŭ v z"
+    arr.sort_by do |word|
+      word.split('').collect do |letter|
         newAlphabetArr.index(letter)
       end
     end
-  end
+  
 end
